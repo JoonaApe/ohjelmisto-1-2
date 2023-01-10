1b1690a8f81f128530d323851366a1c0630dee7b
JoonaApe <joona.apell@metropolia.fi>
2023-01-10 17:19:59 +0200

    gramma tarkkuus ei ole vielä oikea

--- tehtäväsarja_osa2/teht5.py
+++ tehtäväsarja_osa2/teht5.py
@@ -8,9 +8,10 @@ luotiPaino = 13.3
 naulaPaino = 32 * luotiPaino
 leiviskaPaino = 20 * naulaPaino
 
-kokonaisPaino= (leiviskaPaino*leiviska) + (naulaPaino*naulat) + (luotiPaino*luodit)
+kokonaisPaino = (leiviskaPaino*leiviska) + (naulaPaino*naulat) + (luotiPaino*luodit)
 painoKilo = kokonaisPaino/1000
+painoGram = kokonaisPaino - (math.floor(painoKilo)*1000)
 
 
 
-print('Massa nykymittojen mukaan: ' +str(math.floor(painoKilo))+ ' kilogrammaa')
+print('Massa nykymittojen mukaan: ' +str(math.floor(painoKilo))+ ' kilogrammaa ja ' +str(math.floor(painoGram))+ ' grammaa')
