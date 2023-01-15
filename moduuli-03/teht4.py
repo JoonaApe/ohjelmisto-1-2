d61dea29685a4271377814af458026532102fd08
JoonaApe <joona.apell@metropolia.fi>
2023-01-15 21:36:27 +0200

    valmis

--- moduuli-03/teht4.py
+++ moduuli-03/teht4.py
@@ -1,4 +1,4 @@
-vuosi =int(input('Anna vuosi niin kerron onko se karkausvuosi '))
+vuosi = int(input('Anna vuosi niin kerron onko se karkausvuosi '))
 
 if vuosi % 4 == 0 and vuosi % 100 != 0 or vuosi % 400 == 0 and vuosi % 100 == 0:
     print('Vuosi on karkausvuosi')
