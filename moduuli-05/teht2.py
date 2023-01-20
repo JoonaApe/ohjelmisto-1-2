ccf0ce4f51801b9a48237e02ca14f1b9190d2969
JoonaApe <joona.apell@metropolia.fi>
2023-01-20 14:04:00 +0200

    -

--- moduuli-05/teht2.py
+++ moduuli-05/teht2.py
@@ -3,3 +3,16 @@ Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä
 Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen.
 Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille argumentiksi reverse=True.
 '''
+nums = []
+
+while True:
+    num = input('Anna numeroita tai lopeta painamalla Enter. ')
+
+    if not num:
+        break
+    num = int(num)
+    nums.append(num)
+
+nums.sort(reverse=True)
+for i in range(5):
+    print(nums[i])
