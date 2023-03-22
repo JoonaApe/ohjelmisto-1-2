8c454a678a1ce9d0debdef8a71cd7fb2dee1fd4c
JoonaApe <joona.apell@metropolia.fi>
2023-03-22 13:43:40 +0200

    valmis

--- ohjelmisto2/moduuli-12/teht2.py
+++ ohjelmisto2/moduuli-12/teht2.py
@@ -4,13 +4,14 @@ import requests
 hakusana = input("Anna hakusana: ")
 
 
-pyyntö = "https://api.openweathermap.org/data/2.5/weather?q=" + hakusana+ "&units=metric&lang=fi&appid=26d0a02da6a5c39a839bf04976b49eac"
+pyyntö = "https://api.openweathermap.org/data/2.5/weather?q=" + hakusana+ "&units=metric&lang=FI&appid=26d0a02da6a5c39a839bf04976b49eac"
 
 try:
     vastaus = requests.get(pyyntö)
     if vastaus.status_code==200:
         json_vastaus = vastaus.json()
     print(f"Sijainnin {hakusana} lämpötila: {json_vastaus['main']['temp']} °C")
+    print(f"Sään kuvaus: {json_vastaus['weather'][0]['description']}")
 
 except requests.exceptions.RequestException as e:
     print ("Hakua ei voitu suorittaa.")
